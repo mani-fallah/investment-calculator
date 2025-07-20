7d00633f140d98bb301e2409e55c1a181338d462
mani-fallah <mani.f.s.2004@gmail.com>
2025-07-20 12:44:28 +0330

    local storage added

--- src/app/dataserviece.ts
+++ src/app/dataserviece.ts
@@ -1,19 +1,28 @@
-import { Injectable } from '@angular/core';
+import { Injectable,OnInit } from '@angular/core';
 import { Investment} from './data.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Dataserviece {
- private allData : Investment[] = [] ;
+ private allData : Investment[] =  localStorage.getItem('myData') ? JSON.parse(localStorage.getItem('myData')!) : [] ;
 
  onAddInvestment(input:Investment){
    this.allData.push(input);
+   this.saveData();
  }
+
+
 
  get getData()
  {
    return this.allData;
  }
 
+
+ private saveData()
+ {
+   let data: Investment[] = this.allData;
+   localStorage.setItem('myData', JSON.stringify(data));
+ }
 }
